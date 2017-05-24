314950b73959ca7d3ff4284d24047fd3f4fe13f4
Mike Murray <hellomikemurray@gmail.com>
2017-05-24 09:59:04 -0700

    Updated divider example

--- client/screens/DividerScreen.js
+++ client/screens/DividerScreen.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import { Divider, Section } from "../components"
+import { Divider, Section, ExampleBlock } from "../components"
+import * as PTD from "../../lib/propTypeDefinitions";
 
+const componentProps = [
+  PTD.i18nKeyLabel,
+  PTD.label,
+]
 
 class DividerScreen extends Component {
   render() {
     return (
       <Section title="Checkbox">
-        <Divider />
+        <Section>
+          <Divider />
+          <Divider label="Text In Middle" />
+        </Section>
 
-        <Divider label="Text In Middle" />
+
+        <ExampleBlock componentProps={componentProps}>
+          <Divider />
+        </ExampleBlock>
       </Section>
     )
   }
