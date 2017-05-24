ae29195e1f261f83e4408eefccf78facba0ee221
Mike Murray <hellomikemurray@gmail.com>
2017-05-23 18:47:02 -0700

    Expand checkbox example

--- client/screens/CheckboxScreen.js
+++ client/screens/CheckboxScreen.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
-import { Checkbox, Section } from "../components"
+import { Checkbox, Section, ExampleBlock } from "../components"
+import * as PTD from "../../lib/propTypeDefinitions";
+
+const componentProps = [
+  PTD.i18nKeyLabel,
+  PTD.checked,
+  {
+    ...PTD.onChange,
+    description: "Change callback params (event, isInputChecked)"
+  },
+  PTD.label,
+]
 
 
 class CheckboxScreen extends Component {
+  state = {
+    checked: false
+  }
+
+  handleCheck = (event, isInputChecked) => {
+    this.setState({
+      checked: isInputChecked
+    });
+  }
+
   render() {
     return (
       <Section title="Checkbox">
-        <Checkbox
-          label="Check Box"
-        />
+
+        <Section>
+          <Checkbox
+            label="Check Box"
+            onChange={this.handleCheck}
+          />
+        </Section>
+
+        <ExampleBlock componentProps={componentProps}>
+          <Checkbox
+            label="Check Box"
+          />
+        </ExampleBlock>
       </Section>
     )
   }
