4ded05e116ba323fb7749c1826365d7516995304
Mike Murray <hellomikemurray@gmail.com>
2017-05-25 15:46:56 -0700

    Added slider example

--- client/screens/SliderScreen.js
+++ client/screens/SliderScreen.js
@@ -1,7 +1,39 @@
 import React, { Component, PropTypes } from "react";
-import { Slider } from "/imports/plugins/core/ui/client/components";
+import { Slider, Section, ExampleBlock } from "../components";
 
-class MyComponent extends Component {
+export const sliderProps = [
+  {
+    name: "range",
+    type: "Object",
+    description: "`{ min: Number, max: Number }`"
+  }, {
+    name: "start",
+    type: "Array",
+    description: "Start point for handles"
+  }, {
+    name: "margin",
+    type: "Number",
+    description: "Margin between handles"
+  }, {
+    name: "connect",
+    type: "Boolean",
+    description: "Display a colored bar between the handles"
+  }, {
+    name: "step",
+    type: "Number",
+    description: "Amount to move handles when dragging"
+  }, {
+    name: "onChange",
+    type: "Function",
+    description: "Callback when slider changed<br>`(values, handle, unencoded, tap, positions) => {}`"
+  }, {
+    name: "onSlide",
+    type: "Function",
+    description: "Callback when slider is changing<br>`(values, handle, unencoded, tap, positions) => {}`"
+  }
+]
+
+class SliderScreen extends Component {
   handleChange = (values, handle, unencoded, tap, positions) => {
     console.log("slider changed", values);
   }
@@ -12,17 +44,39 @@ class MyComponent extends Component {
 
   render() {
     return (
-      <Slider
-        range={{ min: 0, max: 5000 }}
-        start={[0, 2000]}
-        margin={50}
-        connect={true}
-        step={50}
-        onChange={this.handleChange}
-        onSlide={this.handleSlide}
-      />
+      <Section title="Slider">
+        <Section>
+          <Slider
+            range={{ min: 0, max: 5000 }}
+            start={[1000, 2000]}
+            margin={50}
+            connect={true}
+            step={50}
+            onChange={this.handleChange}
+            onSlide={this.handleSlide}
+          />
+        </Section>
+        <ExampleBlock
+          componentProps={sliderProps}
+          importStatement={{
+            named: ["Slider"],
+            source: "https://github.com/reactioncommerce/reaction/blob/master/imports/plugins/core/ui/client/components/slider/slider.js"
+          }}
+        >
+          <Slider
+            range={{ min: 0, max: 5000 }}
+            start={[0, 2000]}
+            margin={50}
+            connect={true}
+            step={50}
+            onChange={this.handleChange}
+            onSlide={this.handleSlide}
+          />
+        </ExampleBlock>
+      </Section>
+
     );
   }
 }
 
-export default MyComponent;
+export default SliderScreen;
