abd53ebb254cfc3987f7cc9e7574d4d596c49d60
Mike Murray <hellomikemurray@gmail.com>
2017-05-25 15:04:43 -0700

    Added multiselect component examples

--- client/screens/MultiSelectScreen.js
+++ client/screens/MultiSelectScreen.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
-import { MultiSelect, Section } from "../components"
+import { MultiSelect, Section, ExampleBlock } from "../components"
+import * as PTD from "../../lib/propTypeDefinitions";
 
+export const multiSelectProps = [
+  {
+    name: "multi",
+    type: "Boolean",
+    descripton: "Enable multiple select",
+    control: {
+      type: "switch",
+      value: false
+    }
+  },
+  PTD.onChange,
+  {
+    name: "options",
+    type: "Array[Object]",
+    description: "Array of objects: `[{label: \"\", value: \"\"}]`"
+  },
+  {
+    name: "value",
+    type: "String | Araay[String]",
+    description: "Selected value can be a string, for single select, an array of strings for multi"
+  }
+];
 class MultiSelectScreen extends Component {
+
+  state = {
+    values: ["one"]
+  }
+
   onChange = (values) => {
-    console.log("Values are ", values);
+    this.setState({
+      values
+    });
   }
 
   render() {
@@ -16,9 +46,29 @@ class MultiSelectScreen extends Component {
             {value: "three", label: "Three"}
           ]}
           placeholder={"Please choose"}
-          value={this.props.value}
+          value={this.state.values}
           onChange={this.onChange}
         />
+
+        <ExampleBlock
+          componentProps={multiSelectProps}
+          importStatement={{
+            named: ["MultiSelect"],
+            soruce: "/imports/plugins/core/ui/client/components/multiselect/multiselect.js"
+          }}
+          title="MultiSelect"
+        >
+          <MultiSelect
+            options={[
+              {value: "one", label: "One"},
+              {value: "two", label: "Two"},
+              {value: "three", label: "Three"}
+            ]}
+            placeholder={"Please choose"}
+            value={this.state.values}
+            onChange={this.onChange}
+          />
+        </ExampleBlock>
       </Section>
     );
   }
