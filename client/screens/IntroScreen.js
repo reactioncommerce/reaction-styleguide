7cdfa60814548f597bbea26a699c229b9941d94e
Mike Murray <hellomikemurray@gmail.com>
2017-05-31 19:15:00 -0700

    Added rest off intro
    - Made link color more vibrant
    - Added hover for links in markdown blocks
    - Bump version

--- client/screens/IntroScreen.js
+++ client/screens/IntroScreen.js
@@ -54,6 +54,20 @@ Our goals with the style guide are threefold:
 3. Enable more consistency in our code and design as we move forward.
 
 Our style guide is very much a living and evolving document—this is only the beginning. Transparency and constant iteration are core principles of Reaction Commerce, and in that spirit, we wanted to share our style guide as soon as possible.  Check back often as we continue to iterate and evolve the Reaction Style Guide. Please feel free to hop into our [Design Gitter Room](https://gitter.im/reactioncommerce/design) with any questions or suggestions you may have.
+
+## Who is the style guide for?
+
+### Open Source Community
+
+To better fit their needs, our community will want to refer to this style guide when customizing and extending Reaction. Our guide will also make contributing to Reaction easier by giving users components to work with, as well as design guidelines for how each one should be used.
+
+### Reaction Engineers
+
+The style guide will help us achieve consistency, in both design and code, by documenting the components we use, making it  easier to replicate across Reaction. As we continue to build and evolve Reaction, our guide will allow our engineers to efficiently build new features  that remain consistent with the rest of Reaction.
+
+### Designers
+
+As new designers, contractors, and collaborators contribute to Reaction, they will be able to use the style guide and design system to design quickly and efficiently in a style that remains consistent with Reaction’s design philosophy.
 `
 
 class IntroScreen extends Component {
@@ -70,7 +84,7 @@ class IntroScreen extends Component {
   render() {
     return (
       <Section title="Intro">
-        <div dangerouslySetInnerHTML={this.createMarkup()} />
+        <div className="sg-markdown" dangerouslySetInnerHTML={this.createMarkup()} />
       </Section>
     )
   }
