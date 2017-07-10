4c05960dc32d12e091ce7128ebb5a0a912f94997
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-07-10 18:57:00 -0400

    update Circle scripts to handle Mongo container for test run

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -7,11 +7,15 @@ DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-"reactioncommerce/reaction-styleguide"}
 cd $BUILD_DIR/reaction
 
 # build new image
-docker build -t $DOCKER_NAMESPACE:latest .
+docker build --build-arg TOOL_NODE_FLAGS="--max-old-space-size=4096" -t reactioncommerce/reaction-styleguide:latest .
 
 # run the container and wait for it to boot
-docker run --name styleguide -p 3000:3000 -d $DOCKER_NAMESPACE:latest
+docker-compose -f .circleci/docker-compose.yml up -d
 sleep 30
 
 # use curl to ensure the app returns 200's
-docker exec styleguide curl --retry 10 --retry-delay 10 -v http://localhost:3000
+docker exec styleguide bash -c "apt-get update && apt-get install -y curl && \
+  curl --retry 10 --retry-delay 10 -v http://localhost:3000"
+
+# now change the image tag to the configured name
+docker tag reactioncommerce/reaction-styleguide:latest $DOCKER_NAMESPACE:latest
--- .circleci/install.sh
+++ .circleci/install.sh
@@ -10,6 +10,11 @@ mv /tmp/docker/* /usr/bin
 docker -v
 
 
+# install Docker Compose
+curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
 # install Reaction
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
